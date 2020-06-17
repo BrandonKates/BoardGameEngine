aa824e53428149a765b465b32f0206ca71673b9d
Brandon Kates <bjk224@cornell.edu>
2020-06-16 21:10:15 -0400

    added more backgammon logic

--- Backgammon/Backgammon.py
+++ Backgammon/Backgammon.py
@@ -8,11 +8,6 @@ class Color(Enum):
 	DARK = 1
 	EMPTY = 2
 
-class Piece(NamedTuple):
-	type: PieceType
-	color: Color
-
-
 class BoardSquare(NamedTuple):
 	color: Color
 	num: int
@@ -22,17 +17,19 @@ class Direction(Enum):
 	DARK: -1
 
 
-class Move(NamedTuple): # pass is encoded (-1,-1,false)
+class Move(NamedTuple): # pass is encoded (None,roll,false)
 	color: Color
 	pos: int # 0-23, bar=self.bar_pos
 	num: int # roll: 1-6
 	hit: bool # hit piece of opposite color
 
 def direction_from_color(color):
-	if color == Color.WHITE:
+	if color == Color.LIGHT:
 		return 1
 	elif color == Color.DARK:
 		return -1
+
+
 
 class Backgammon:
 	def __init__(self):
@@ -50,7 +47,7 @@ class Backgammon:
 		self.beared_pieces = {Color.LIGHT: 0, Color.DARK: 0} # pieces that have moved off the board
 
 	def ind_to_points(self, ind):
-		return ind + 1
+		return str(ind + 1)
 
 	def roll_die(self):
 		return random.randint(1,6)
@@ -69,7 +66,7 @@ class Backgammon:
 		return self.at(point).num
 
 	def in_board(self, ind):
-		return 0 < ind < self.board_size
+		return 0 <= ind < self.board_size
 
 	def is_empty(self, point):
 		return self.color_at(point) == Color.EMPTY
@@ -124,10 +121,82 @@ class Backgammon:
 	#	hit: bool # hit piece of opposite color
 
 	def get_pos(self, pos, roll, color): # roll is a single value here
+		if pos == self.bar_pos:
+			pos = self.bar_start_ind(color)
 		return pos + direction_from_color(color) * roll
 
-	def get_pos_from_bar(self, roll, color):
-		return self.get_pos(self.bar_start_ind(color), roll, color)
+	def get_pos_from_move(self, move):
+		return self.get_pos(move.pos, move.num, move.color)
+
+	# Backgammon Algebraic Notation = BAN
+	def move_to_BAN(self, move : Move) -> str:
+		initial_pos = 'bar' if move.pos == self.bar_pos else self.ind_to_points(move.pos)
+
+		fp = self.get_pos_from_move(move)
+		final_pos = self.ind_to_points(fp) if self.in_board(fp) else 'off'
+		#BAN_notation = '%s/%s' % (initial_pos, final_pos)
+		#if move.hit:
+		#BAN_notation += '*'
+		#return BAN_notation
+		return initial_pos, final_pos, move.hit
+
+	def moves_to_BAN_no_doubles(self, moves) -> str:
+		rolls = []
+		for move in moves:
+			rolls.append(move.roll)
+			if move.pos is None:
+
+	def moves_to_BAN(self, moves) -> str:
+		n = len(moves)
+		if len(moves) <= 2:
+			self.moves_to_BAN_no_doubles(moves)
+		else:
+			self.moves_to_BAN_doubles(moves)
+
+	def combine_multimoves(self, moves: List) -> List:
+		 # given two moves:
+		 # either combine them as 6-2:9/3 3/1 -> 6-2:9/1
+		 # or if a hit then: 6-2:9/3* 3/1 -> 6-2:9/3*/1
+		 # else: 2-2: 9/5 9/5 -> 2-2:9/5(2)
+
+	def move_equality(self, m1, m2):
+		return m1.pos == m2.pos and m1.num == m2.num:
+
+	def check_shared(self, m1, m2):
+		m1_final = self.get_pos_from_move(m1)
+		m2_final = self.get_pos_from_move(m2)
+		hit = m1.hit or m2.hit
+
+		if m1_final == m2.pos:
+			if m1.hit:
+				# new move is m1.pos/m1_final*/m2_final
+				(m1.pos, m1_final, m2_final), (False, True, False)
+			elif m2.hit:
+				(m1.pos, m1_final, m2_final), (False, False, True)
+				# m1.pos/m2_final*
+			else:
+				(m1.pos, m1_final, m2_final), (False, False, False)
+				# m1.pos/m2_final
+		elif m2_final == m1.pos:
+			if m2.hit:
+				(m2.pos, m2_final, m1_final), (False, True, False)
+				# new move is m2.pos/m2_final*/m1_final
+			elif m1.hit:
+				(m2.pos, m2_final, m1_final), (False, False, True)
+				# new move is m2.pos/m1_final*
+			else:
+				(m2.pos, m2_final, m1_final), (False, False, False)
+				# new move is m2.pos/m1_final
+
+	def combine_two(self, move1, move2):
+		max_die = max(move1.num, move2.num)
+		min_die = min(move1.num, move2.num)
+
+		if self.get_pos_from_move(move1)
+		# check same (last):
+		elif self.move_equality(move1, move2):
+			s = '%s-%s: %s/%s(2)'
+			#return Move(move1.pos, move1.num, move1.hit or move2.hit) # same move
 
 
 	# Update Board to reflect the move, i.e. move out of bar, or bear, or move one spot to another, captures, etc...
@@ -137,15 +206,12 @@ class Backgammon:
 		start_pos = move.pos
 		roll = move.num
 		hit = move.hit
-		if start_pos == self.bar_pos:
-			end_pos = self.get_pos_from_bar(move.num, self.cur_player)
-		else:
-			end_pos = self.get_pos(move.pos, move.roll, self.cur_player)
+		end_pos = self.get_pos(move.pos, move.num, self.cur_player)
 		if not self.in_board(end_pos): # bear move
 			self.beared_pieces[self.cur_player] += 1
 		else:
 			# not bear move
-
+			return
 		self.cur_player 
 
 
@@ -157,12 +223,12 @@ class Backgammon:
 		if self.bar[color] > 0:
 			# must move both out, don't consider any other pieces
 			start_ind = self.bar_start_ind(color)
-			if self.is_legal_spot(start_ind + roll[0], color) and self.is_legal_spot(start_ind + roll[1], color)
+			if self.is_legal_spot(start_ind + roll[0], color) and self.is_legal_spot(start_ind + roll[1], color):
 				return move_to_notation() 
 
 
 	def moves_double(self, color, roll):
-
+		pass
 
 	def get_legal_moves(self, color, roll):
 		direction = direction_from_color(color)
