e50a81386b00d278e6869483087fd6e38468b3b0
Brandon Kates <bjk224@cornell.edu>
2021-04-02 13:17:30 -0400

    fix scoreboard

--- Othello/react-othello/src/components/Scoreboard/Scoreboard.tsx
+++ Othello/react-othello/src/components/Scoreboard/Scoreboard.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import './Scoreboard.scss';
 
 export default function Scoreboard(props: any) {
-  const whiteScore = props.score[0];
-  const blackScore = props.score[1];
-
   return (
   <div className="scoreboard">
     <div className="score-header"> Score </div>
@@ -13,8 +10,8 @@ export default function Scoreboard(props: any) {
         <div className="score-color">{"⚫"}</div>
     </div>
     <div className="score-row">
-        <div className="score">{whiteScore}</div>
-        <div className="score">{blackScore}</div>
+        <div className="score">{props.score["⚪"]}</div>
+        <div className="score">{props.score["⚫"]}</div>
     </div>
   </div>
 );
